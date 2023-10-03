06ab872014eb0f2a2792c72f642a8f37cb9d4fab
Jale Sh <jale.shikhaliyeva@gmail.com>
2023-10-03 12:39:51 +0400

    add a js file

---
+++ question_answer.js
@@ -0,0 +1,95 @@
+var q1 = confirm ("18 yasiniz olub?")
+
+if ( q1==true ) {
+    console.log("devam ede bilersiniz");
+
+    var q2 = prompt ("ilde nece ay var?");
+    if ( q2 =="12" ) {
+        console.log("sizin xal :10");
+          
+    var q3 = prompt ("hansi ayda 28 gun var?");  
+    if ( q3 == "butun aylarda" ) {
+        console.log("sizin xal :20");
+
+    var q4 = prompt ("ilde nece fesil var?");
+    if ( q4 == "4" ) {
+        console.log("sizin xal :30");
+
+    var q5 = prompt ("sentyabr ilin necenci ayidir?");
+    if ( q5 == "9" ) {
+        console.log("sizin xal:40");
+    
+    var q6 = prompt ("dekabr ilin necenci ayidir?");
+    if ( q6 == "12") {
+        console.log("sizin xal:50");
+
+    var q7 = prompt ("3cu ay hansi aydir?");
+    if (q7 == "mart" ) {
+        console.log("sizin xal:60");
+
+    var q8 = prompt ("hal hazirda necenci esrdeyik");
+    if (q8 == "21" ) {
+        console.log("sizin xal:70");
+
+    var q9 = prompt ("iyul ilin necenci ayidir?");
+    if ( q9 == "7" ) {
+        console.log("sizin xal:80");
+
+    var q10 = prompt ("necenci ildeyik?");
+    if ( q10 == "2023") {
+        console.log("sizin xal : 90");
+
+    var q11 = prompt ("her fesilde nece ay var?");
+    if ( q11 == "3" ) {
+        console.log("sizin xal : 100");
+    } else {
+        console.log("sizin xal:90");
+    }
+
+
+    } else {
+        console.log("80");
+    }
+    
+
+    } else {
+        console.log("sizin xal:70");
+    }
+
+    } else {
+        console.log("sizin xal :60");
+    }
+
+    } else {
+        console.log("sizin xal:50");
+    }
+
+
+    } else {
+        console.log("sizin xal:40");
+    }
+
+
+    } else {
+        console.log("sizin xal:30");
+    }
+
+
+    } else {
+        console.log("sizin xal :20");
+    }
+
+
+    } else {
+        console.log("sizin xal :10");
+    }
+
+
+
+    } else {
+        console.log("sizin xal :0");
+    }
+
+} else {
+    console.log("teesufki,istirak ede bilmezsiniz");
+}
